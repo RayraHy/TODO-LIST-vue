8ec960728fcb29a17bfb5c7c84ca9c71a8614956
Rayra <rayra.hyasmymb@gmail.com>
2023-05-11 22:52:20 -0300

    +

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="task-grid">
+    <template v-if="tasks.length">
+      <TaskItem v-for="(task, i) in tasks" :key="i" :task="task" @taskDeleted="$emit('taskDeleted', i)" @taskStateChanged="$emit('taskStateChanged', i)" />
+    </template>
+    <p v-else class="no-task">Sua vida esta em dia :)</p>
+  </div>
+</template>
+
+<script>
+import TaskItem from './TaskItem.vue'
+
+export default {
+    name: 'task-grid',
+    components: {
+      TaskItem
+    },
+    props: {
+      tasks: {
+        type: Array,
+        require: true
+      }
+    }
+}
+</script>
+
+<style scoped>
+  .task-grid {
+    display: flex;
+    /*flex-direction: column;*/
+    justify-content: center;
+    flex-wrap: wrap;
+    /*gap: 20px;*/
+  }
+
+  .task {
+    margin: 10px;
+  }
+
+  no-task {
+    font-size: 1.7rem;
+  }
+</style>
---
+++ src/components/TaskProgress.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tasks-progress">
+    <span class="progress-value">{{ progress }}%</span>
+    <div class="progress-bar"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'tasks-progress',
+    props: {
+        type: Number,
+        default: 0
+    }
+}
+</script>
+
+<style scoped>
+    .tasks-progress {
+        position: relative;
+        width: 80%;
+        border: 1px solid white;
+        color: white;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .progress-bar {
+        position: absolute;
+        border-radius: 8px;
+        height: 100%;
+        background-color: #4caf50;
+        align-self: flex-start;
+        
+    }
+
+    .progress-value {
+        z-index: 1;
+        font-size: 1.5rem;
+    }
+</style>
